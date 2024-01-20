ce4977b04d70c7b3d361b001337191eec5769eb3
Ryan Lam <mrsssprite@gmail.com>
2024-01-20 16:17:14 +0800

    a few inClass examples in Chap 11

---
+++ Chapter_11/inClass/fgets2.c
@@ -0,0 +1,19 @@
+/**
+ * @file    fgets2.c
+ * @brief   another example using fgets and fputs
+ */
+#include <stdio.h>
+
+#define STLEN 10
+
+int main(void)
+{
+  char words[STLEN];
+
+  puts("Enter strings (empty line to quit):");
+  while (fgets(words, STLEN, stdin) != NULL && words[0] != '\n')
+    fputs(words, stdout);
+  puts("Done.");
+
+  return 0;
+}
