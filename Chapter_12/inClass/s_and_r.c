a255a9020db2c6ebdb060223c5ec078f00a7bbc8
Ryan Lam <ryanspritelam@outlook.com>
2024-01-21 21:32:35 +0800

    added some random related functions

---
+++ Chapter_12/inClass/s_and_r.c
@@ -0,0 +1,18 @@
+/**
+ * @file    s_and_r.c
+ * @brief   file for rand1 and srand1; using ANSI C portable algorithm
+ */
+
+static unsigned long int next = 1;
+
+int rand1(void)
+{
+  /* magic formula to generate pseudorandom number */
+  next = next * 1103515245 + 12345;
+  return (unsigned int) (next / 65536) % 32768;
+}
+
+void srand1(unsigned int seed)
+{
+  next = seed;
+}
